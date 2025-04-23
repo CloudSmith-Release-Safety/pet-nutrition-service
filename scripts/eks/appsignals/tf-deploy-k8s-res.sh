f7b96c330537590b1c92eabd28dfc4166f24fc0d
Yong Tao <117978688+taoyong-ty@users.noreply.github.com>
2025-04-22 20:08:28 -0700

    Update tf-deploy-k8s-res.sh

--- scripts/eks/appsignals/tf-deploy-k8s-res.sh
+++ scripts/eks/appsignals/tf-deploy-k8s-res.sh
@@ -29,4 +29,4 @@ endpoint=$(kubectl get ingress -o json  --output jsonpath='{.items[0].status.loa
 
 # Force rollout restart of all deployments to ensure they pick up the latest images
 echo "Forcing rollout restart of all deployments to ensure they use the latest images"
-kubectl rollout restart deployment/insurance-service-python --namespace=$NAMESPACE
+kubectl rollout restart deployment/nutrition-service-nodejs --namespace=$NAMESPACE
